5e920e0100da349104708c835307afdcf768e9e2
Sebastian Rojas <jhsrojasro@unal.edu.co>
2021-12-06 20:48:55 -0500

    Added recommendation Querys and Mutations

--- api_gateway/schema/__init__.py
+++ api_gateway/schema/__init__.py
@@ -6,10 +6,12 @@ import graphene
 # Crowstream API Gateway
 from .user import *
 from .support import *
+from .recommendation import *
 
 
 class Query(
-    user.account.resolvers.Query, 
+    user.account.resolvers.Query,
+    recommendation.resolvers.Query, 
     support.post.resolvers.Query, 
     support.support_request.resolvers.Query
 ):
@@ -19,9 +21,10 @@ class Query(
 
 class Mutation(
     support.post.resolvers.Mutation,
-    support.support_request.resolvers.Mutation
+    support.support_request.resolvers.Mutation,
+    recommendation.resolvers.Mutation
 ):
     """Class to join all mutations resolvers in a simple class."""
-
+    pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
